ee1b116f27a9c1164443b0b75d9c67356ad1aeb2
magrigor <maxim.agrigoroae@endava.com>
2020-09-23 13:25:06 +0300

    Add logging

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import ApiArticleHostingImporter from './api';
+import config from './api/config';
 import LoggerService, { Level } from './api/service/logger.service';
 
 void (async (): Promise<void> => {
-  console.log(process.env.NODE_ENV, 'environment');
+  console.log(JSON.stringify(config, undefined, 2), 'config');
 
   let code = 0;
   const logger = new LoggerService();
